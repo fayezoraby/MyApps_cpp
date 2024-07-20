12e8852e0275c950e48a03bfdff5636c44efe90d
Fayez <fayezeloraby@icloud.com>
2024-07-20 20:25:40 +0400

    hi

---
+++ ofc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << (10 == 10) << "\n";     //true => 1
+    cout << (10 > 10) << "\n";      //false => 0
+    cout << (10 < 10) << "\n";      //false => 0
+    cout << (10 >= 10) << "\n";     //true 10 = 10 => 1
+    cout << (10 <= 10) << "\n";     //true => 1
+    cout << (10 != 10) << "\n";     //false => 0
+    
+
+    return 0;
+}
