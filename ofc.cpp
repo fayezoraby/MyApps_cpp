718937cbfe94beac6c043e0050d5f109f8d84f8a
Fayez <fayezeloraby@icloud.com>
2024-07-21 19:33:00 +0400

    ofc file for control flow

--- ofc.cpp
+++ ofc.cpp
@@ -4,13 +4,52 @@ using namespace std;
 
 int main()
 {
-    cout << (10 == 10) << "\n";     //true => 1
-    cout << (10 > 10) << "\n";      //false => 0
-    cout << (10 < 10) << "\n";      //false => 0
-    cout << (10 >= 10) << "\n";     //true 10 = 10 => 1
-    cout << (10 <= 10) << "\n";     //true => 1
-    cout << (10 != 10) << "\n";     //false => 0
-    
+    int sum = 20 + 5 * 4 / 2 - 2;
+    cout << "1 - 5*4/2 = 10" << endl;
+    cout << "             ^" << endl;
+    cout << "2 - 20 +    10 - 2 = 28" << endl; 
+    cout << "\nThe final sum is : " << sum;
     return 0;
-
 }
+
+
+
+
+// int main()
+// {
+//     int age;
+//     cout << "How old are you ? \n";
+//     cin >> age;
+//     cout << endl;
+//     if (age < 18)
+//     {
+//         cout << "you are not old enough!\n";
+//     }
+//     else if (age >= 18)
+//     {
+//         cout << "You are fit for this !\n";
+//     }
+//     else
+//     {
+//         cout << "Enter a valid age! \n";
+
+//     }
+
+
+//     return 0;
+// }
+
+
+
+// int main()
+// {
+//     cout << (10 == 10) << "\n";     //true => 1
+//     cout << (10 > 10) << "\n";      //false => 0
+//     cout << (10 < 10) << "\n";      //false => 0
+//     cout << (10 >= 10) << "\n";     //true 10 = 10 => 1
+//     cout << (10 <= 10) << "\n";     //true => 1
+//     cout << (10 != 10) << "\n";     //false => 0
+    
+//     return 0;
+
+// }
