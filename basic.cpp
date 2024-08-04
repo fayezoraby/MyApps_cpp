15dd5a004c631bb155d1b7b433eb1b309b43fffe
Fayez <fayezeloraby@icloud.com>
2024-08-04 21:30:45 +0400

    "new basics.cpp file"

---
+++ basic.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+
+{
+    std::cout << "Ethar is the love of my life!";
+    return 0;
+
+}
---
+++ basics.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main(){
+
+    std::cout << "I like bizza!" << std::endl;
+    std::cout << "It's very good!" << std::endl;
+    
+    return 0;
+}
