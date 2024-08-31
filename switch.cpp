ed92955f8238e5b9fc86e9f8fd8d8880a1d7c011
Fayez Eloraby <88905943+fayezoraby@users.noreply.github.com>
2024-08-31 07:09:48 +0000

    updated switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -7,48 +7,43 @@ using std::cin;
 using std::endl;
 using std::string;
 
-
-int main(){
-    int month;
-    cout << "Enter the number of month (1-12) : ";
-    cin >> month;
-
-    switch (month){
+void monthCheck(int month){
+switch (month){
         case 1:
-            cout << "January" << endl;
+            cout << "The 1st month of the year is January" << endl;
             break;
         case 2:
-            cout << "February" << endl;
+            cout << "The 2nd month of the year is February" << endl;
             break;
         case 3:
-            cout << "March" << endl;
+            cout << "The 3rd month of the year is March" << endl;
             break;
         case 4:
-            cout << "April" << endl;
+            cout << "The 4th month of the year is April" << endl;
             break;
         case 5:
-            cout << "May" << endl;
+            cout << "The 5th month of the year is May" << endl;
             break;
         case 6:
-            cout << "June" << endl;
+            cout << "The 6th month of the year is June" << endl;
             break;
         case 7:
-            cout << "July" << endl;
+            cout << "The 7th month of the year is July" << endl;
             break;
         case 8:
-            cout << "August" << endl;
+            cout << "The 8th month of the year is August" << endl;
             break;
         case 9:
-            cout << "September" << endl;
+            cout << "The 9th month of the year is September" << endl;
             break;
         case 10:
-            cout << "October" << endl;
+            cout << "The 10th month of the year is October" << endl;
             break;
         case 11:
-            cout << "November" << endl;
+            cout << "The 11th month of the year is November" << endl;
             break;
         case 12:
-            cout << "December" << endl;
+            cout << "The 12th month of the year is December" << endl;
             break;
         default:
             cout << "Invalid input. Enter in a valid month number ( 1 - 12).\n";
@@ -56,6 +51,23 @@ int main(){
 
 
     }
+}
 
+int main(){
+    int month;
+    while (true){
+        cout << "Enter the number of month (1-12) : ";
+        if (cin >> month && month >= 1 && month <= 12 ) {
+            break;
+        } else {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Please inter a valid month number (1-12).\n";
+        }
+    }
+
+    cout << "\n";
+    monthCheck(month);
+    cout << "\n";
     return 0;
 }
